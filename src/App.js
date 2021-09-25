4b730dae0281c71f2457b376cea933f693c11b3b
Rocio Diaz <rociodiaz91@hotmail.com>
2021-09-25 13:12:03 -0300

    cambio de titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Poroyecto React Rocio Diaz
+          Proyecto React Rocio Diaz
         </p>
         <a
           className="App-link"
