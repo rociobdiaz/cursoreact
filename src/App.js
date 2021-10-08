14ad4fbd0db19c19b39e21340d5a85f11fa2297a
Rocio Diaz <rociodiaz91@hotmail.com>
2021-10-07 21:28:58 -0300

    se agrego bootstrap sass e iconos react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './components/Container/ItemListContainer';
 
